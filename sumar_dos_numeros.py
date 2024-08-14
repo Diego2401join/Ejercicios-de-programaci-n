7be174df8f9441aecd9933d2188ca5c462aa1ea1
Diego Alejandro Molina <diegoa.molinaa@uqvirtual.edu.co>
2024-08-14 00:13:31 -0500

    Programa para sumar

---
+++ sumar_dos_numeros.py
@@ -0,0 +1,8 @@
+nombre = input("como te llamas ")
+a = input("hola, " + nombre + " dame un numero ")
+
+b = input("dame otro numero ")
+A = float(a)
+B = float(b)
+c = A + B
+print("la suma de estos dos numeros es ", c)
